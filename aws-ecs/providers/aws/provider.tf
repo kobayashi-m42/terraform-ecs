50b1c44e28ec65660cadd6018d9f2826b2a76a82
kobayashi-m42 <m42kobayashi@gmail.com>
2019-12-28 12:53:32 +0900

    :arrow_up: #5 aws providerを最新に更新

--- aws-ecs/providers/aws/provider.tf
+++ aws-ecs/providers/aws/provider.tf
@@ -1,7 +1,7 @@
 variable "profile" {}
 
 provider "aws" {
-  version = "=2.4.0"
+  version = "=2.43.0"
   profile = "${var.profile}"
   region  = "${lookup(var.common, "${terraform.env}.region", var.common["default.region"])}"
 }
